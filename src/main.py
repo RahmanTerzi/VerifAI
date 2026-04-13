22bf228e7a7351887f6bfdae54805ceeb4becbc1
faruk9870 <dalkilic342@gmail.com>
2026-04-13 23:17:18 +0300

    Ui tasarımı oluşturuldu

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
-# Proje giriş noktası
-print('VerifAI Başlatılıyor...')
+import sys
+from PyQt5.QtWidgets import QApplication
+from ui_manager import UIManager
+
+app = QApplication(sys.argv)
+
+window = UIManager()
+window.show()
+
+sys.exit(app.exec_())
